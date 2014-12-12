66b2ede66e1cd8a7c1d9c89e457db55b446954cd
ncuillery <nicolas.cuillery@gmail.com>
2014-12-12 16:03:34 +0100

    Fresh generation from `yo angular`

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularBreadcrumbIssue55App
+ * @description
+ * # angularBreadcrumbIssue55App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularBreadcrumbIssue55App', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBreadcrumbIssue55App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularBreadcrumbIssue55App
+ */
+angular.module('angularBreadcrumbIssue55App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
